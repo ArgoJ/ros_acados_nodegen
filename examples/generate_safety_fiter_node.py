d1bcaba52b824635a403172fdd2a4183625be769
Josua Lindemann <josua.lindemann@outlook.de>
2025-08-19 00:47:28 +0200

    added set acados solver constraint and weights

--- examples/generate_safety_fiter_node.py
+++ examples/generate_safety_fiter_node.py
@@ -7,6 +7,7 @@ def main():
         solver_path=Path(__file__).parent / "scripts" / "c_generated_code" / "safety_filter_ocp.json",
         install_path=Path(__file__).parent,
         config_path=Path(__file__).parent / "safety_filter_node_config.yaml",
+        script_path=Path(__file__).parent / "scripts" / "safety_filter_mpc.py",
     )
 
 if __name__ == "__main__":
